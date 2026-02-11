bb4708ef04a3ac57b03c929848d82797593156ff
SGeethaathman <geethaathmansrinivasan@gmail.com>
2026-02-12 01:28:54 +0530

    Fix dynamic port for Render

--- app3.py
+++ app3.py
@@ -428,5 +428,5 @@ def view_business():
 # RUN APPLICATION
 # =====================================================
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=10000)
 
